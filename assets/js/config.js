fa5371b3fbc655a09a735a0eaac4dd291ff11217
norshaida-JKR <305695387+norshaida-JKR@users.noreply.github.com>
2026-07-29 16:22:06 +0800

    config: sambungkan website kepada Google Apps Script

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,5 @@
 window.ASET_CONFIG = {
-  // Tampal URL deployment Google Apps Script selepas deploy sebagai Web App.
- API_URL: "TAMPAL_URL_APPS_SCRIPT_DI_SINI",
-USE_DEMO_DATA: false,
+  API_URL: "https://script.google.com/macros/s/AKfycbzL2AtjUxL0_KA0aQAeWkUoIcMOxuONaa2By5MMUDZosbtxGxi1EpjYYDo4wqCRpB60Ow/exec",
+  USE_DEMO_DATA: false,
   APP_NAME: "ASET MPE",
 };
