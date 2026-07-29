dce648629aec6b3be63b2a490c0ed344d8d599bf
norshaida-JKR <norshaida@jkr.gov.my>
2026-07-29 16:19:54 +0800

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 window.ASET_CONFIG = {
   // Tampal URL deployment Google Apps Script selepas deploy sebagai Web App.
-  API_URL: "",
-  USE_DEMO_DATA: true,
+ API_URL: "TAMPAL_URL_APPS_SCRIPT_DI_SINI",
+USE_DEMO_DATA: false,
   APP_NAME: "ASET MPE",
 };
